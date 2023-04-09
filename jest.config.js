0a64aecfcc245e4b501a03f85f138960ccead00c
Arthur Ferreira <fepi.arthur@gmail.com>
2023-04-09 00:00:02 -0300

    refactor: :recycle: update test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/'],
+    testPathIgnorePatterns: ['/node_modules/', 'dist'],
     setupFiles: ['dotenv/config'],
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    }
   };
   
